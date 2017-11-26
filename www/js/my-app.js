76130c8673b1a662278a969b8f2c7169dd9ca5f4
kyoshiroaquino <ellisaquino72@yahoo.com>
2017-11-26 01:34:31 -0800

    Add files via upload

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -1,22 +1,16 @@
-// Initialize app
 var myApp = new Framework7();
-
-
-// If we need to use custom DOM library, let's save it to $$ variable:
 var $$ = Dom7;
 
-// Add view
+
 var mainView = myApp.addView('.view-main', {
-    // Because we want to use dynamic navbar, we need to enable it for this view:
+   
     dynamicNavbar: true
 });
 
-// Handle Cordova Device Ready Event
+
 $$(document).on('deviceready', function() {
     console.log("Device is ready!");
 });
-
-
 
 
 
@@ -25,7 +19,7 @@ myApp.onPageInit('about', function (page) {
 
 })
 
-// Option 2. Using one 'pageInit' event handler for all pages:
+
 $$(document).on('pageInit', function (e) {
     // Get page data from event data
     var page = e.detail.page;
